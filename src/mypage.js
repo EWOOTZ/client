603946207b35cb17d82361486b65caf6618579f5
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2024-08-07 01:21:06 +0900

    feat. 마이페이지 완료

--- src/mypage.js
+++ src/mypage.js
@@ -21,31 +21,41 @@ function Mypage() {
                     <button className="login-gray" style={{ fontSize: "20px" }} onClick={() => navigate()}>변경</button>
                 </div>
                 <div style={{ height: "3vh" }}></div>
-                <div className='grey-box'>
-                    <div className='hang'>
-                        <p style={{ color: "black", fontSize: '20px' }}>ID</p>
-                        <div style={{ width: '1vw' }}></div>
-                        <input className='input-name' type='text' placeholder='아이디를 입력하세요.' />
+                <div className='grey-box2'>
+                    <div style={{width : "20vw"}}></div>
+                    <div className='hang3'>
+                        <p style={{ color: "black", fontSize: '20px' }}>닉네임</p>
+                        <div style={{ width: '6.7vw' }}></div>
+                        <input className='input-2' type='text' placeholder='닉네임을 입력하세요...' />
                     </div>
                     <div style={{ height: '5vh' }}></div>
                     <div className='hang'>
-                        <p style={{ color: "black", fontSize: '20px' }}>PW</p>
-                        <div style={{ width: '1vw' }}></div>
-                        <input className='input-name' type='text' placeholder='패스워드를 입력하세요.' />
+                        <p style={{ color: "black", fontSize: '20px' }}>프로필 뮤직</p>
+                        <div style={{ width: '3.3vw' }}></div>
+                        <input className='input-2' type='text' placeholder='프로필 뮤직을 설정하세요...' />
+                        <div style={{width:"0.5vw"}}></div>
+                        <button className="login-gray" style={{ fontSize: "15px" }} onClick={() => navigate()}>검색</button>
                     </div>
                     <div style={{ height: '5vh' }}></div>
                     <div className='hang'>
-                        <p style={{ color: "black", fontSize: '13px' }}>확인</p>
-                        <div style={{ width: '0.7vw' }}></div>
-                        <input className='input-name' type='text' placeholder='패스워드를 재입력하세요.' />
+                        <p style={{ color: "black", fontSize: '20px' }}>한줄 소개</p>
+                        <div style={{ width: '4.6vw' }}></div>
+                        <input className='input-2' type='text' placeholder='한줄 소개를 입력하세요...' />
                     </div>
                     <div style={{ height: '5vh' }}></div>
                     <div className='hang'>
-                        <p style={{ color: "black", fontSize: '13px' }}>이름</p>
-                        <div style={{ width: '0.7vw' }}></div>
-                        <input className='input-name' type='text' placeholder='이름을 입력하세요.' />
+                        <p style={{ color: "black", fontSize: '20px' }}>내 미니홈피 URL</p>
+                        <div style={{ width: '0.5vw' }}></div>
+                        <input className='input-2' type='text' placeholder='http://minihomep.com/haejin0221' />
+                        <div style={{width:"0.5vw"}}></div>
+                        <button className="login-gray" style={{ fontSize: "15px" }} onClick={() => navigate()}>복사</button>
                     </div>
                 </div>
+                <div style={{height : "1vh"}}></div>
+                <div className='hang'>
+                    <div style={{width : "55vw"}}></div>
+                <button className="login-gray" style={{ fontSize: "20px" }} onClick={() => navigate()}>수정하기</button>
+            </div>
             </div>
         </div>
     );
