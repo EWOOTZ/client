3cc2d2539940d4fe237d7ef0562921f3c861b952
sudinggg <115580334+sudinggg@users.noreply.github.com>
2024-08-14 18:45:31 +0900

    feat. 메인 만드는 중

--- src/main.js
+++ src/main.js
@@ -5,15 +5,33 @@ import { useState } from 'react';
 import picturetrash from './assets/trash.png';
 import pictureHome from './images/Oak Tree.png';
 import pictureapple from './images/apple.png';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
+
+function formatDate() {
+    const today = new Date();
+    
+
+    const day = today.getDate(); 
+    const month = today.getMonth() + 1; 
+    const year = today.getFullYear(); 
+    
+    const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+    const dayOfWeek = daysOfWeek[today.getDay()]; // 요일
+
+    return `${year % 100}.${month}.${day}(${dayOfWeek})`;
+}
 
 function Main() {
     const [isExiting, setIsExiting] = useState(false);
     const [showPopup, setShowPopup] = useState(false);
     const [text, setText] = useState("");
 
-    const handleTrashClick = () => {
+    let title = '토비의\n';
+    let title2 = '마이홈피';
 
+    const handleTrashClick = () => {
         setText("");
         setShowPopup(true);
     };
@@ -22,7 +40,7 @@ function Main() {
         setIsExiting(true);
         setTimeout(() => {
             setShowPopup(false);
-            setIsExiting(false); 
+            setIsExiting(false);
         }, 500);
     };
 
@@ -30,17 +48,38 @@ function Main() {
         setText(event.target.value);
     };
 
-    let title = '<토비>의\n';
-    let title2 = '마이홈피';
-
     const navigate = useNavigate();
+
     return (
         <div className='backg'>
             <div className='white-line'>
-                <button className="login-gray" style={{ fontSize: "20px" }} onClick={() => navigate(`/mypage/${localStorage.getItem("id")}`)}>마이페이지</button>
-                <button className="login-gray" style={{ fontSize: "20px" }} onClick={() => navigate(`/ground/${localStorage.getItem("id")}`)}>광장가기</button>
-                <img src={picturetrash} width='80vw' height='70vh' alt="trash" onClick={handleTrashClick} style={{ cursor: 'pointer' }} /> 
+            <button className="go-ground-gray" style={{ fontSize: "25px" }} onClick={() => navigate(`/ground/${localStorage.getItem("id")}`)}>광장가기</button>
+            <div className='container'>
+                    <h1 className='date-text'>{formatDate()}</h1>
+                    <div className='title-text'>
+                    <p style={{ color: '#00DAC0'}}>
+          {title}
+        </p>
+        <p style={{ color: '#8A8A8A'}}>
+          {title2}
+        </p>
+                    </div>
+                </div> <div className='container'>
+                <div className='main-white-box'>
+                    <button className="login-gray" style={{ fontSize: "20px" }} onClick={() => navigate(`/mypage/${localStorage.getItem("id")}`)}>마이페이지</button>
+                </div>
+                <div className='main-gray-box'>
+                </div>
+                <div className='column-container'>
+                <div className='main-transparent-box'>
+                </div>
+                <div className='trash-image-container'>
+                <img src={picturetrash} className='trash-image' alt="trash" onClick={handleTrashClick} style={{ cursor: 'pointer' }} />                </div>
+                </div>
+
             </div>
+
+
             {showPopup && (
                 <div className={`letter-popup ${isExiting ? 'exiting' : ''}`}>
                     <div className="letter-popup-content">
@@ -59,6 +98,7 @@ function Main() {
                     </div>
                 </div>
             )}
+             </div>
         </div>
     );
 }
